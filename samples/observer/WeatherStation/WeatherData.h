595fe999653aab3271de8c9c165730d700df685c
eligantRU <eligant.ru@gmail.com>
2018-09-27 21:44:53 +0300

    Устранил дублирование при отображении статистических данных

--- samples/observer/WeatherStation/WeatherData.h
+++ samples/observer/WeatherStation/WeatherData.h
@@ -1,11 +1,54 @@
 ﻿#pragma once
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <climits>
 #include "Observer.h"
 
 using namespace std;
+
+class CStats
+{
+public:
+	double GetMin() const
+	{
+		return m_minValue;
+	}
+
+	double GetMax() const
+	{
+		return m_maxValue;
+	}
+
+	void AddValue(double value)
+	{
+		m_minValue = std::min(m_minValue, value);
+		m_maxValue = std::max(m_maxValue, value);
+
+		m_accValue += value;
+		++m_countAcc;
+	}
+
+	double GetAverage() const
+	{
+		return m_accValue / m_countAcc;
+	}
+
+private:
+	double m_minValue = std::numeric_limits<double>::infinity();
+	double m_maxValue = -std::numeric_limits<double>::infinity();
+	double m_accValue = 0;
+	unsigned m_countAcc = 0;
+};
+
+void PrintStats(ostream& stream, const CStats& stats, const string& index)
+{
+	stream << "Max " << index << " " << stats.GetMax() << endl;
+	stream << "Min " << index << " " << stats.GetMin() << endl;
+	stream << "Average " << index << " " << stats.GetAverage() << endl;
+	stream << "----------------" << endl;
+}
 
 struct SWeatherInfo
 {
@@ -23,10 +66,10 @@ private:
 	*/
 	void Update(SWeatherInfo const& data) override
 	{
-		std::cout << "Current Temp " << data.temperature << std::endl;
-		std::cout << "Current Hum " << data.humidity << std::endl;
-		std::cout << "Current Pressure " << data.pressure << std::endl;
-		std::cout << "----------------" << std::endl;
+		cout << "Current Temp " << data.temperature << endl;
+		cout << "Current Hum " << data.humidity << endl;
+		cout << "Current Pressure " << data.pressure << endl;
+		cout << "----------------" << endl;
 	}
 };
 
@@ -39,28 +82,18 @@ private:
 	*/
 	void Update(SWeatherInfo const& data) override
 	{
-		if (m_minTemperature > data.temperature)
-		{
-			m_minTemperature = data.temperature;
-		}
-		if (m_maxTemperature < data.temperature)
-		{
-			m_maxTemperature = data.temperature;
-		}
-		m_accTemperature += data.temperature;
-		++m_countAcc;
+		m_tempStat.AddValue(data.temperature);
+		m_humidityStat.AddValue(data.humidity);
+		m_pressureStat.AddValue(data.pressure);
 
-		std::cout << "Max Temp " << m_maxTemperature << std::endl;
-		std::cout << "Min Temp " << m_minTemperature << std::endl;
-		std::cout << "Average Temp " << (m_accTemperature / m_countAcc) << std::endl;
-		std::cout << "----------------" << std::endl;
+		PrintStats(std::cout, m_tempStat, "Temp");
+		PrintStats(std::cout, m_humidityStat, "Humidity");
+		PrintStats(std::cout, m_pressureStat, "Pressure");
 	}
 
-	double m_minTemperature = std::numeric_limits<double>::infinity();
-	double m_maxTemperature = -std::numeric_limits<double>::infinity();
-	double m_accTemperature = 0;
-	unsigned m_countAcc = 0;
-
+	CStats m_tempStat;
+	CStats m_humidityStat;
+	CStats m_pressureStat;
 };
 
 class CWeatherData : public CObservable<SWeatherInfo>
